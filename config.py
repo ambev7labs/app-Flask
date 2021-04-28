5764eef4dcdafe9f8043a635736841c552eb70ce
Gabriel <gabriel.campos9815@gmail.com>
2021-04-27 22:12:23 -0300

    update db

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 SECRET_KEY = 'alura'
-MYSQL_HOST = "localhost"
-MYSQL_USER = "root"
+MYSQL_HOST = "https://remotemysql.com/phpmyadmin/index.php"
+MYSQL_USER = "ns3059021"
 MYSQL_PASSWORD = "admin"
-MYSQL_DB = "jogoteca"
+MYSQL_DB = "5369F6lH6T"
 MYSQL_PORT = 3306
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
